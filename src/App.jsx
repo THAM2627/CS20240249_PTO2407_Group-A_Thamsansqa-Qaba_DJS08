d94e44cd16d561ebdf1d16242fe768171ad638f7
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-13 01:41:53 +0200

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,31 @@
  * Review challenge: do all the CSS yourself based on the design
  * linked in the slides.
  */
- 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from ".pages/Home";
+import About from ".pages/About";
+import App from './App.jsx';
+import './index.css';
+
 function App() {
 
   return (
-    <h1>Start here</h1>
+  <BrowserRouter>
+      <header>
+      <nav>
+        <Link to="/">#VANLIFE</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      </header>  
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+
+  </BrowserRouter>
   )
 }
 
