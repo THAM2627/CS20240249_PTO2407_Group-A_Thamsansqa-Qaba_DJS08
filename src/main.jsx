461fe8ae08182d75a723b720439a7700703ec0dd
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-13 01:05:08 +0200

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from ".pages/Home";
+import About from ".pages/About";
+import App from './App.jsx';
+import './index.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
