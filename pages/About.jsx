818158a18da16fbcb755e88b62a3fbcb416a79b2
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-13 02:35:17 +0200

    about page update

--- pages/About.jsx
+++ pages/About.jsx
@@ -2,6 +2,13 @@ import React from "react";
 
 export default function About() {
     return (
-        <h1>About</h1>
-    )
+        <div className="about-container">
+        <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
+        <p>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch. (Hitch costs extra 😉)</p>
+        <p>Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
+
+
+        </div>
+
+    );
 }
