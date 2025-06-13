74bdeddce51f15dd80540955e9bb55a240391188
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-13 23:18:17 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,9 @@ import Layout from './Components/Layout';
 import"./server"
 import './index.css';
 import Dashboard from './pages/Host/Dashboard';
+import Income from './pages/Host/Income';
+import Reviews from './pages/Host/Reviews';
+import HostLayout from './Components/HostLayout';
 
 function App() {
 
@@ -42,6 +45,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
         <Route path="/vans/:id" element={<VanDetail />} />
+        <Route path="/host" element={<HostLayout />} />
         <Route path= "/host" element= {<Dashboard />} />
         <Route path="/host/income" element={<Income />} />
         <Route path="/host/reviews" element={<Reviews />} />
--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { Outlet } from "react-router";
 
 export default function Dashboard() {
     return (
+        <div>
         <h1>Dashboard</h1>
+        < Outlet />
+        </div>
     )
 }
