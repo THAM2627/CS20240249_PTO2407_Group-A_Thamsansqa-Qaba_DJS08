b427ab21e42bd687f16e8a50e918e0cb7f546fe4
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-13 22:43:35 +0200

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Vans from "./pages/Vans";
-import VanDetail from "./pages/VanDetail";
+import Vans from "./pages/Vans/Vans";
+import VanDetail from "./pages/Vans/VanDetail";
 import Layout from './Components/Layout';
 import"./server"
 import './index.css';
+import Dashboard from './pages/Host/Dashboard';
 
 function App() {
 
@@ -36,11 +37,15 @@ function App() {
       </header>  
 
       <Routes>
+
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
         <Route path="/vans/:id" element={<VanDetail />} />
-        
+        <Route path= "/host" element= {<Dashboard />} />
+        <Route path="/host/income" element={<Income />} />
+        <Route path="/host/reviews" element={<Reviews />} />
+
       </Routes>
 
   </BrowserRouter>
