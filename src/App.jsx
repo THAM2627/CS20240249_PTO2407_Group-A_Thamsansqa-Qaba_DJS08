a401ad8abcfd2492dd55f3663f023e59ba253a68
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-13 16:30:38 +0200

    van detail update

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,11 @@
  * linked in the slides.
  */
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "./pages/Vans";
+import VanDetail from "./pages/VanDetail";
 import"./server"
 import './index.css';
 
@@ -38,6 +38,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
         
       </Routes>
 
---
+++ src/pages/VanDetail.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function VanDetail() {
+    return 
+        <h1> Van Detail Page</h1>
+}
