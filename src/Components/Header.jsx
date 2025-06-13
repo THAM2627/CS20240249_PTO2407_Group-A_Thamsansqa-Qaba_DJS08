530116ca644e1e1f189dabba8f625e89564f7440
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-13 23:22:20 +0200

    header and footer update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Header() {
+    const activeStyles = {
+        fontWeight: "bold",
+        color: "#161616",
+        textDecoration: "underline"
+    }
     return (
         <header>
         <Link className="site-logo" to="/">#VanLife</Link>
