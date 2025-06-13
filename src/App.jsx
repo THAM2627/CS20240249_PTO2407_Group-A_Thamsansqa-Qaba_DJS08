8db63f2fe370a3290afb7058281538292562dbf9
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-13 15:51:32 +0200

    adding mirage dependices

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "./pages/Vans";
+import"./server"
 import './index.css';
 
 function App() {
--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
-function Vans() {
+export default function Vans() {
+    React.useEffect(() => {
+        fetch("/api/vans")
+        .then (res => res.json())
+        .then(data=>console.log(data))
+    }, [])
+
+    console.log(vans)
+    
     return <div>
         Vans
         
         </div>;
 }
 
-export default Vans
