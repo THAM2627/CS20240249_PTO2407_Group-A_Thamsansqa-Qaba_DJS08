517739aaf4ce58c5188f6422bad618a943278f27
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-14 23:24:12 +0200

    Update AuthRequired.jsx

--- src/Components/AuthRequired.jsx
+++ src/Components/AuthRequired.jsx
@@ -2,9 +2,9 @@ import React from "react"
 import { Outlet, Navigate } from "react-router-dom"
 
 export default function AuthRequired() {
-    const authenticated = false
+    const isLoggedIn = localStorage.getItem("loggedin")
     
-    if (!authenticated) {
+    if (!isLoggedIn) {
         return (<Navigate 
             to="/login"
         state={{ message: "You must log in to view this page" }}
