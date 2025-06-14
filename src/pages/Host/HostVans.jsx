e981acbe82344c4da39d1500410d404c1eb5b752
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-14 03:21:18 +0200

    Update HostVans.jsx

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function HostVans() {
+function HostVans() {
+    
+    const [vans, setVans] = React.useState([])
+
+    React.useEffect(() => {
+        fetch("/api/host/vans")
+        .then(res => res.json())
+        .then(data => setVans(data.vans))
+    }, [])
+
+    
     return (
         <h1>HostVans</h1>
     )
 }
+
+export default HostVans
