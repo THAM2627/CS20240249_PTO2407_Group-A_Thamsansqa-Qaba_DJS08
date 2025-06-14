13c1bf415636781a27fe5f81b1de8491f5efe4cd
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-14 21:19:28 +0200

    Update HostVans.jsx

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -39,7 +39,14 @@ function HostVans() {
         </div>
         </Link>
     ))
+
+    if(loading){
+        return <h1>Loading...</h1>
+    }
     
+    if (error){
+        return<h1>There was an error:{error.message}</h1>
+    }
     return (
         <section>
             <h1 className="host-vans-title">Your listed vans</h1>
