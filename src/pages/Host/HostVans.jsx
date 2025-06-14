9f40c079c941c94286772a562dc9089e57cc72a2
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-14 21:15:38 +0200

    Update HostVans.jsx

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {getHostVans} from "../../api"
 
 function HostVans() {
 
     const [vans, setVans] = React.useState([])
+    const [loading, setLoading] = React.useState(true)
+    const [error, setError] = React.useState(null)
 
     React.useEffect(() => {
-        fetch("/api/host/vans")
-        .then(res => res.json())
-        .then(data => setVans(data.vans))
+        async function loadVans() {
+            setLoading(true)
+            try{
+                const data = await getHostVans()
+                setVans(data)
+            } catch (err) {
+                setError(err)
+            } finally {
+                setLoading(false)
+            }
+            loadVans()
+        }
     }, [])
 
     const hostVansEls = vans.map(van=> (  
