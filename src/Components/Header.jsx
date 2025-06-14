2fd77978d867b87242a2689fc6913fc2541d0b01
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-14 23:25:46 +0200

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,6 +7,10 @@ function Header() {
         color: "#161616",
         textDecoration: "underline"
     }
+    function fakeLogOut() {
+        localStorage.removeItem("loggedin")
+    }
+    
     return (
         <header>
         <Link className="site-logo" to="/">#VanLife</Link>
