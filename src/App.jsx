adc2a5f45db908f5dd537d07d1bde0808e2eb2d0
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-14 04:00:15 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,26 @@ function App() {
 
       <Routes>
 
- 
+      <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />
+              }/>
+
+              </Route>
+              </Route>
+            </Route>
       </Routes>
 
   </BrowserRouter>
