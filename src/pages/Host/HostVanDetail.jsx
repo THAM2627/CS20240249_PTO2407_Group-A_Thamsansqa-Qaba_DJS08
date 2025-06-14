ed3f1a93c351bc8988fcecd32dcdae513f1a5c03
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-14 02:11:51 +0200

    Update HostVanDetail.jsx

--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -2,6 +2,27 @@ import React from "react";
 import { useParams, Link, NavLink, Outlet } from "react-router-dom";
 
 function HostVanDetail() {
+    const { id } = useParams()
+    const [currentVan, setCurrentVan] = React.useState(null)
+
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+
+    React.useEffect(() => {
+        fetch(`/api/host/vans/${id}`)
+        .then(res => res.json())
+        .then(data => setCurrentVan(data.van))
+    }, [])
+
+    if (!currentVan) {
+        return (
+            <h1>Loading...</h1>
+        )   
+    }
+
     return (
         <h1>HostVanDetail</h1>
     )
