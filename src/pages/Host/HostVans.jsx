f97499ce64de4ab2207de562b0261b3ec99bbdb8
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-14 03:29:16 +0200

    Update HostVans.jsx

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -16,7 +16,16 @@ function HostVans() {
         to={`/host/vans/${van.id}`} 
         key={van.id}
         className="host-van-link-wrapper">
-
+        
+        <div className="host-van-single" key={van.id}>
+            <img src={van.imageUrl} alt={`Photo of ${van.name}`}/>
+            
+        <div className="host-van-info">
+            <h3>{van.name}</h3>
+            <p>${van.price}<span>/day</span></p>
+            </div>
+        </div>
+        </Link>
     ))
     
     return (
