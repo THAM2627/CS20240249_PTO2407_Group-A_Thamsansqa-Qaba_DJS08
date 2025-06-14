6edc87b57dd5607b40949e62c842dd064cbcb17d
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-14 02:25:52 +0200

    Update HostVanDetail.jsx

--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -24,7 +24,27 @@ function HostVanDetail() {
     }
 
     return (
-        <h1>HostVanDetail</h1>
+        <section>
+        <Link
+            to=".."
+            relative="path"
+            className="back-button"
+        >&larr; <span>Back to all vans</span></Link>
+
+        <div className="host-van-detail-layout-container">
+        <div className="host-van-detail">
+        <img src={currentVan.imageUrl} />
+        <div className="host-van-detail-info-text">
+        <i
+        className={`van-type van-type-${currentVan.type}`}>
+        {currentVan.type}
+        </i>
+        <h3>{currentVan.name}</h3>
+        <h4>${currentVan.price}/day</h4>
+        </div>
+            </div>
+        
+
     )
 }
 
