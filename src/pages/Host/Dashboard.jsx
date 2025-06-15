90fcc15ee1a8e73162b9b5b69c18ae7bef83f730
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-15 19:37:55 +0200

    Update Dashboard.jsx

--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import {getHostVans} from "../../api"
+import { BsStarFill } from "react-icons/bs"
 
 
 function Dashboard() {
@@ -6,7 +9,7 @@ function Dashboard() {
     const [loading, setLoading] = React.useState(false)
     const [error, setError] = React.useState(null)
 
-    React.useEffect() => {
+    React.useEffect(); {
         setLoading(true)
         getHostVans()
         .then(data => {
@@ -17,14 +20,49 @@ function Dashboard() {
         })
         .finally(() => {
             setLoading(false)
-        })
-    }
+        },[])
+    
+
+ function renderVanElements(vans) {
+    const hostVansEls = vans.map(van=> (
+        <div className="host-van-single" key={van.id}>
+            <img src={van.imageUrl} alt={`Photo of ${van.name}`}/>
+            <div className="host-van-info">
+                <h3>{van.name}</h3>
+                <p>${van.price}/day</p>
+            </div>
+            <Link to={`/host/vans/${van.id}`}>View Van </Link>
+    ))
     return (
-        <div>
-        <h1>Dashboard</h1>
-˜
+        <div className="host-vans-list">
+            <section>
+                {hostVansEls}
+            </section>
         </div>
     )
+
+
+if (error) { 
+    return(<h1>There was an error:{error.message}</h1>)
 }
+
+return(
+    <>
+    <section className="host-dashboard-earnings">
+
+        <div className="info">
+            <h1>Welcome</h1>
+            <p>Last 30 days</p>
+            <h2>$2,260</h2>
+        </div>
+
+        <div className="reviews">
+            <h2>Reviews</h2>
+            <div className="star">
+                <BsStarFill />
+    </div>
+    </section>
+    </>
+)
 
 export default Dashboard
