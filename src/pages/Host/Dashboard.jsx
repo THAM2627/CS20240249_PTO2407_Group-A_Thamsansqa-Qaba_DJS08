915b9b73c7a9e028428fb6491842bc04b050f0f6
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-15 18:47:57 +0200

    Update Dashboard.jsx

--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -2,6 +2,23 @@ import React from "react";
 
 
 function Dashboard() {
+    const [vans, setVans] = React.useState([])
+    const [loading, setLoading] = React.useState(false)
+    const [error, setError] = React.useState(null)
+
+    React.useEffect() => {
+        setLoading(true)
+        getHostVans()
+        .then(data => {
+            setVans(data)
+        })
+        .catch(err => {
+            setError(err)
+        })
+        .finally(() => {
+            setLoading(false)
+        })
+    }
     return (
         <div>
         <h1>Dashboard</h1>
